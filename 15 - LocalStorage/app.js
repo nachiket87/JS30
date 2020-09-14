a941d941caa6b06c3b27eda7b0da844146242dc8
Nachiket Pusalkar <nachiket87@gmail.com>
2020-09-14 16:28:05 -0400

    unsolvable error

--- 15 - LocalStorage/app.js
+++ 15 - LocalStorage/app.js
@@ -1,11 +1,44 @@
-"use strict";
-var addItems = document.querySelector('.add-items');
-var itemsList = document.querySelector('.plates');
-var items = [];
-items = [{ id: 1 }, { id: 2 }, 3];
+const addItems = document.querySelector('.add-items');
+const itemsList = document.querySelector('.plates');
+const items = JSON.parse(localStorage.getItem('items')) || [];
+
 function addItem(e) {
-    console.log(e);
-    e.preventDefault();
+  e.preventDefault();
+  const text = (this.querySelector('[name=item]')).value;
+  const item = {
+    text,
+    done: false
+  };
+
+  items.push(item);
+  populateList(items, itemsList);
+  localStorage.setItem('items', JSON.stringify(items));
+  this.reset();
 }
+
+function populateList(plates = [], platesList) {
+  platesList.innerHTML = plates.map((plate, i) => {
+    return `
+      <li>
+        <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''} />
+        <label for="item${i}">${plate.text}</label>
+      </li>
+    `;
+  }).join('');
+}
+
+function toggleDone(e) {
+  if (!e.target.matches('input')) return; // skip this unless it's an input
+  const el = e.target;
+  const index = el.dataset.index;
+  items[index].done = !items[index].done;
+  localStorage.setItem('items', JSON.stringify(items));
+  populateList(items, itemsList);
+}
+
 addItems.addEventListener('submit', addItem);
-// console.log(typeof(addItems)); is an object
+itemsList.addEventListener('click', toggleDone);
+
+populateList(items, itemsList);
+
+
--- 15 - LocalStorage/app.ts
+++ 15 - LocalStorage/app.ts
@@ -1,23 +1,42 @@
 export {}
-
 const addItems = document.querySelector('.add-items');
-const itemsList = document.querySelector('.plates');
-let items: {}[] = [];
+const itemsList: HTMLUListElement= document.querySelector('.plates');
+const items = JSON.parse(localStorage.getItem('items')) || [];
 
-items = [{id: 1}, {id: 2}, 3]
 
 
 function addItem(e: Event) {
-  console.log(e);
-  e.preventDefault();
+  e.preventDefault(); // prevent reload of page which is default for form subission (submit button)
+
+  const text: string = (this.querySelector('[name=item]')).value
+  
+  let item = {
+    text,
+    done: false,
+  };
+  items.push(item);
+  populateList(items, itemsList);
+  localStorage.setItem('items', JSON.stringify(item))
+  this.reset();
 }
 
+const populateList = (plates = [], platesList: HTMLUListElement) => {
+  platesList.innerHTML = plates.map((plate, i: number)  => {
+    return `
+    <li>
+      <input type="checkbox" data-index=${i} id=item${i} ${plate.done ? 'checked' : ''} />
+      <label for="item${i}">${plate.text}</label>
+    </li>
+    `
+  }).join("");
+
+}
 addItems.addEventListener('submit', addItem);
+populateList(items, itemsList);
 
 
 
 
-// console.log(typeof(addItems)); is an object
 
 
 
